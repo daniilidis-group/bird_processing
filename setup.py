68c5870d266121e0510420074b8d90bb175aeff4
Bernd Pfrommer <bernd.pfrommer@gmail.com>
2019-12-16 09:41:48 -0500

    added setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name='birdprocessing',
+      version='1.0',
+      description='Bird Video Processing Utilities',
+      author='Bernd Pfrommer',
+      author_email='bernd.pfrommer@gmail.com',
+      url='https://github.com/daniilidis-group/bird_processing',
+      packages=find_packages()
+     )
